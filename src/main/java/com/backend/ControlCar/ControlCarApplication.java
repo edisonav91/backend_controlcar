90ed08630db18b6c75d6d76b7201463b52843189
Edison Alvarez <edisonav1991@gmail.com>
2024-09-17 13:01:43 -0500

    Confirmación Inicial

---
+++ src/main/java/com/backend/ControlCar/ControlCarApplication.java
@@ -0,0 +1,13 @@
+package com.backend.ControlCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlCarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/ControlCar/ControlCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.ControlCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
