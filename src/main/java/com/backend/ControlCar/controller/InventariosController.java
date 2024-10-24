c33c1af306a61ecd1b08ece5c2b517fa6e162b4e
Edison Alvarez <edisonav1991@gmail.com>
2024-10-24 15:12:40 -0500

    se realizo eliminar de Inventario

--- src/main/java/com/backend/ControlCar/controller/InventariosController.java
+++ src/main/java/com/backend/ControlCar/controller/InventariosController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 
@@ -21,12 +23,24 @@ public class InventariosController {
 
     @GetMapping("/")
     public String lista(Model model) {
-        // Obtengo los datos vehiculo de la BD
+        // Obtengo los datos inventario de la BD
         ArrayList<Inventario> inventarios = (ArrayList<Inventario>) inventarioRepository.findAll();
 
-        // Mando la lista de estados a la vista
+        // Mando la lista de inventario a la vista
         model.addAttribute("inventario", inventarios);
 
         return "inventarios/lista";
+
+    }
+        @GetMapping("/eliminar/{id}")
+        public String eliminar(@PathVariable("id") Integer id, RedirectAttributes redirectAttributes) {
+            // Verificar si la marca existe
+            if (inventarioRepository.existsById(id)) {
+                inventarioRepository.deleteById(id);
+                redirectAttributes.addFlashAttribute("mensaje", "Inventario eliminado con éxito.");
+            } else {
+                redirectAttributes.addFlashAttribute("error", "Inventario no encontrada.");
+            }
+            return "redirect:/inventario/";
     }
 }
