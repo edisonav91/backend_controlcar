6d29378ff420ba9a6eb19a291a1ae04179504dcd
Edison Alvarez <edisonav1991@gmail.com>
2024-10-24 20:38:53 -0500

    Se creo el eliminar de Documento

--- src/main/java/com/backend/ControlCar/controller/EstadosController.java
+++ src/main/java/com/backend/ControlCar/controller/EstadosController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 
@@ -26,5 +28,18 @@ public class EstadosController {
         model.addAttribute("estados", estados);
 
         return "estados/lista";
+
+        }
+        @GetMapping("/eliminar/{id}")
+        public String eliminar(@PathVariable("id") Integer id, RedirectAttributes redirectAttributes) {
+            // Verificar si la marca existe
+            if (estadoRepository.existsById(id)) {
+                estadoRepository.deleteById(id);
+                redirectAttributes.addFlashAttribute("mensaje", "Estado eliminada con éxito.");
+            } else {
+                redirectAttributes.addFlashAttribute("error", "Estado no encontrada.");
+            }
+            return "redirect:/estado/";
     }
+
 }
