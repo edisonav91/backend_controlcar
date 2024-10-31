f107d8fd72e16f3a403e901568c9e48bc3502005
Edison Alvarez V <edisonav1991@gmail.com>
2024-10-30 21:24:10 -0500

    Guardar marca

--- src/main/java/com/backend/ControlCar/controller/MarcasController.java
+++ src/main/java/com/backend/ControlCar/controller/MarcasController.java
@@ -5,9 +5,7 @@ import com.backend.ControlCar.repository.MarcaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
@@ -40,5 +38,23 @@ public class MarcasController {
             redirectAttributes.addFlashAttribute("error", "Marca no encontrada.");
         }
         return "redirect:/marcas/";
+    }
+
+    @PostMapping("/guardar")
+    public String guardarMarca(@ModelAttribute Marca marca, RedirectAttributes redirectAttributes) {
+        try {
+            marcaRepository.save(marca);
+            redirectAttributes.addFlashAttribute("mensaje", "Marca agregada con éxito.");
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("error", "Hubo un problema al guardar la marca.");
+        }
+        return "redirect:/marcas/";
+    }
+
+    @GetMapping("/crear")
+    public String crear(Model model) {
+        ArrayList<Marca> marcas = (ArrayList<Marca>) marcaRepository.findAll();
+        model.addAttribute("marca", new Marca());
+        return "marcas/crear";
     }
 }
